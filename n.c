97785bb26f2f6d93aadfab2427a7ad53cc479c9e
sughosh-panigrahi <sughosh.panigrahi@gmail.com>
2026-08-20 12:28:50 +0530

    Add files via upload

---
+++ n.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+int n;
+printf("the number is %d",n);
+scanf("%d",&n);
+int sum =0 ;
+for(i=1;i=n;i++){
+sum += i;
+}
+printf("the sum of n natural numbers is %d",n,sum);
+return 0;
+}
