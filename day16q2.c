972670dedf2ed905b055cf2ea59c934f33266be2
sughosh-panigrahi <sughosh.panigrahi@gmail.com>
2026-08-27 10:47:06 +0530

    Add files via upload

---
+++ day16q2.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+#include<string.h>
+int main(){
+int num;
+int remainder;
+int reversednum=0;
+int original;
+printf("the number is :%d",num);
+scanf("%d",&num);
+original=num;
+while(num!=0){
+remainder =reversednum*10+remainder;
+num/=10;
+}
+if (original==reversednum){
+printf("the number is a palindrome :%d",original);
+}
+else{printf(" the number is not a palindrome :%d",original);
+}
+return 0;
+}
+
+
