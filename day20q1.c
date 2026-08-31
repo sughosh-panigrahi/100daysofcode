57e7456c13079672e3fa707e3cc83c661a3df894
sughosh-panigrahi <sughosh.panigrahi@gmail.com>
2026-08-31 22:15:49 +0530

    Add files via upload

---
+++ day20q1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+int num;
+printf("Enter a number :%d",num);
+scanf("%d",&num);
+if(num %2 !=0 ){
+num%2!=0;
+printf("the entered number is an odd number :");
+}
+return 0;
+}
