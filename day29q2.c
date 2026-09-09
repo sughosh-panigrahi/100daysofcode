fbbda58b14189b26e21457e86578b1e9e054bf3a
sughosh-panigrahi <sughosh.panigrahi@gmail.com>
2026-09-09 12:33:39 +0530

    Add files via upload

---
+++ day29q2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+int arr[] = {10, 5, 25, 8, 15};
+int n = sizeof(arr) / sizeof(arr[0]);
+int max = arr[0];
+int min = arr[0];
+for (int i = 1; i < n; i++) {
+if (arr[i] > max)
+max = arr[i];
+if (arr[i] < min)
+min = arr[i];
+}
+printf("Maximum element = %d\n", max);
+printf("Minimum element = %d\n", min);
+return 0;
+}
