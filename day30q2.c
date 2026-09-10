aaa934cc7a79a1ab6262131451b9e72dc9b0147f
sughosh-panigrahi <sughosh.panigrahi@gmail.com>
2026-09-10 12:53:06 +0530

    Add files via upload

---
+++ day30q2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+int n, i;
+int positive = 0, negative = 0, zero = 0;
+printf("Enter the number of elements: ");
+scanf("%d", &n);
+int arr[n];
+printf("Enter the array elements:\n");
+for(i = 0; i < n; i++) {
+scanf("%d", &arr[i]);
+}
+for(i = 0; i < n; i++) {
+if(arr[i] > 0)
+positive++;
+else if(arr[i] < 0)
+negative++;
+else
+zero++;
+}
+printf("Positive elements = %d\n", positive);
+printf("Negative elements = %d\n", negative);
+printf("Zero elements = %d\n", zero);
+return 0;
+}
