31a76b17950183f8073992daf4759a76920aa012
sughosh-panigrahi <sughosh.panigrahi@gmail.com>
2026-09-10 12:52:46 +0530

    Add files via upload

---
+++ day30q1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+int arr[100], n, i;
+int even = 0, odd = 0;
+printf("Enter the number of elements: ");
+scanf("%d", &n);
+printf("Enter the array elements:\n");
+for (i = 0; i < n; i++) {
+scanf("%d", &arr[i]);
+if (arr[i] % 2 == 0)
+even++;
+else
+odd++;
+}
+printf("Number of even elements = %d\n", even);
+printf("Number of odd elements = %d\n", odd);
+return 0;
+}
