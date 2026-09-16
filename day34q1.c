54d18be54b43c7c289ba758dd15a48e932b36e09
sughosh-panigrahi <sughosh.panigrahi@gmail.com>
2026-09-16 12:34:24 +0530

    Add files via upload

---
+++ day34q1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+int arr[100], n, element, pos, i;
+printf("Enter number of elements: ");
+scanf("%d", &n);
+printf("Enter array elements:\n");
+for (i = 0; i < n; i++)
+scanf("%d", &arr[i]);
+printf("Enter element to insert: ");
+scanf("%d", &element);
+printf("Enter position (1 to %d): ", n + 1);
+scanf("%d", &pos);
+if (pos < 1 || pos > n + 1) {
+printf("Invalid position.");
+return 0;
+}
+for (i = n; i >= pos; i--)
+arr[i] = arr[i - 1];
+arr[pos - 1] = element;
+n++;
+printf("Array after insertion:\n");
+for (i = 0; i < n; i++)
+printf("%d ", arr[i]);
+return 0;
+}
